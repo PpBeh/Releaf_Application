46e9dfd886ec32166853b5bc9879d1586a635406
BEH-spPrgm <behjz-pm25@student.tarc.edu.my>
2026-07-27 20:45:13 +0800

    Initial commit

--- app/src/main/java/com/example/releaf/MainActivity.kt
+++ app/src/main/java/com/example/releaf/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ReleafTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Android 123",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
