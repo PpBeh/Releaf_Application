4536ff3ba9785e13dfb65babbc278d9c261d4da3
Jeremy <jeremychng1980@gmail.com>
2026-07-29 14:41:10 +0800

    Test Commit

--- app/src/main/java/com/example/releaf/MainActivity.kt
+++ app/src/main/java/com/example/releaf/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             ReleafTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Beh super smart",
+                        name = "Michael Goh Shin Yong",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
